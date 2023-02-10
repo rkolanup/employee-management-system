c9ca4b63b9a20c389184d26ac23d847805bee863
Ranganayaki Kolanupaka <93740204+rkolanup@users.noreply.github.com>
2023-02-10 11:21:02 -0500

    upload test folder

---
+++ api/test/employee.e2e-spec.ts
@@ -0,0 +1,9 @@
+import * as request from 'supertest';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+describe('EmployeeModule (e2e)', () => {
+    //const baseUrl = `http://localhost:${process.env.PORT}/api`;
+
+})
